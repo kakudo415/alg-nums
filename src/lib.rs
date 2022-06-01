51c4906221ca75c1c8e35726cec9a0293ced84c9
kakudo415 <kakudo415@gmail.com>
2022-06-01 19:49:58 +0900

    少しaddテスト

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,24 @@ mod tests {
     use super::integer::Integer;
 
     #[test]
-    fn test_add_sub() {
-        // TODO: 目視・手動検算じゃないテストを書く
-        let mut ans = Integer::from(100000000);
-        for i in 0..200 {
-            println!("{:03}: {:X}", i, ans);
-            ans = &ans + &ans;
-        }
+    fn test_add() {
+        let v0 = Integer::zero();
+        let v9 = Integer::from(9);
+        let v16 = Integer::from(16);
+        let v25 = Integer::from(25);
+        assert!(&v9 + &v16 == v25);
+        assert!(&v0 + &v25 == v25);
 
-        let mut ans = Integer::from(-100000000);
-        for i in 0..200 {
-            println!("{:03}: {:X}", i, ans);
-            ans = &ans + &ans;
+        let mut ans9 = Integer::zero();
+        let mut ans16 = Integer::zero();
+        for _ in 0..256 {
+            for _ in 0..16 {
+                ans9 = &ans9 + &v9;
+            }
+            for _ in 0..9 {
+                ans16 = &ans16 + &v16;
+            }
+            assert!(ans9 == ans16);
         }
     }
 }
