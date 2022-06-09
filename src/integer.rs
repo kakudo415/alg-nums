f4e3e38e84803b93ff6aebae64f84dd1a3d9f321
kakudo415 <kakudo415@gmail.com>
2022-06-10 04:32:38 +0900

    自然数と同じようにファイルをまとめた

--- src/integer.rs
+++ src/integer.rs
@@ -1,12 +1,12 @@
 pub mod add;
 pub mod cmp;
-pub mod misc;
 pub mod sub;
 
 // 自然数を拡張して整数を作る
 pub use super::natural::*;
 
-use std::ops::Neg;
+use std::fmt;
+use std::ops;
 
 #[derive(Copy, Clone, PartialEq)]
 enum Sign {
@@ -20,7 +20,6 @@ pub struct Integer {
     abs_value: Natural,
 }
 
-// immutableにすれば、Copyにしてもいい？
 impl Integer {
     fn new(capacity: usize) -> Self {
         Integer {
@@ -53,7 +52,7 @@ impl Clone for Integer {
     }
 }
 
-impl Neg for &Integer {
+impl ops::Neg for &Integer {
     type Output = Integer;
 
     fn neg(self) -> Integer {
@@ -64,5 +63,37 @@ impl Neg for &Integer {
             Sign::Minus => Sign::Plus,
         };
         neg_integer
+    }
+}
+
+impl ops::Index<usize> for Integer {
+    type Output = usize;
+    fn index(&self, idx: usize) -> &usize {
+        &self.abs_value[idx]
+    }
+}
+
+impl ops::IndexMut<usize> for Integer {
+    fn index_mut(&mut self, idx: usize) -> &mut usize {
+        &mut self.abs_value[idx]
+    }
+}
+
+impl From<isize> for Integer {
+    fn from(value: isize) -> Self {
+        if value == 0 {
+            return Integer::zero();
+        }
+        let mut new_integer = Integer::new(1);
+        new_integer.sign = if value > 0 { Sign::Plus } else { Sign::Minus };
+        new_integer[0] = value.abs() as usize;
+        new_integer
+    }
+}
+
+impl fmt::UpperHex for Integer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let sign = if self.sign == Sign::Minus { "-" } else { " " };
+        write!(f, "{}{:X}", sign, self.abs_value)
     }
 }
