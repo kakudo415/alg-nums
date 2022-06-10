b8430b931cb626cc44605760b3f194b5713b8fa6
kakudo <kakudo415@gmail.com>
2022-06-11 04:31:58 +0900

    かけ算をリファクタリングして、一時変数としてのNaturalを生成しないように

--- src/natural/digit.rs
+++ src/natural/digit.rs
@@ -38,19 +38,19 @@ pub(crate) fn sub_borrow<const N: usize>(operands: [Digit; N]) -> [Digit; 2] {
     [answer, borrow]
 }
 
-pub(crate) fn mul_carry(lhs: Digit, rhs: Digit, carry: Digit) -> [Digit; 2] {
+pub(crate) fn mul_carry(lhs: Digit, rhs: Digit, carry: Digit, inter: Digit) -> [Digit; 2] {
     let lhs_lo = lhs & LOW_BITS;
     let lhs_hi = lhs >> HALF_WIDTH;
     let rhs_lo = rhs & LOW_BITS;
     let rhs_hi = rhs >> HALF_WIDTH;
 
-    let lo = lhs_lo * rhs_lo;
-    let mid_0 = lhs_lo * rhs_hi;
-    let mid_1 = lhs_hi * rhs_lo;
-    let hi = lhs_hi * rhs_hi;
+    let lo = lhs_lo * rhs_lo; // <= 11100001
+    let mid_0 = lhs_lo * rhs_hi; // <= 11100001
+    let mid_1 = lhs_hi * rhs_lo; // <= 11100001
+    let hi = lhs_hi * rhs_hi; // <= 11100001
 
-    let lo = add_carry([lo, mid_0 << HALF_WIDTH, mid_1 << HALF_WIDTH, carry]);
-    let hi = hi + (mid_0 >> HALF_WIDTH) + (mid_1 >> HALF_WIDTH) + lo[1];
+    let lo = add_carry([lo, mid_0 << HALF_WIDTH, mid_1 << HALF_WIDTH, carry, inter]); // <= 00000010 11111111
+    let hi = hi + (mid_0 >> HALF_WIDTH) + (mid_1 >> HALF_WIDTH) + lo[1]; // <= 11111111
 
     [lo[0], hi]
 }
--- src/natural/mul.rs
+++ src/natural/mul.rs
@@ -7,29 +7,23 @@ impl Mul for &Natural {
     type Output = Natural;
 
     // TODO: Karatsuba法・FFT法も実装する
-    fn mul(self, rhs: Self) -> Natural {
-        let mut answer = Natural::new(needed_capacity(self, rhs));
-
-        for i in 0..rhs.length {
-            if rhs[i] == 0 {
-                continue;
-            }
-            answer = &answer + &mul_1digit(self, rhs[i], i);
-        }
-
-        answer.normalize();
-        answer
+    fn mul(self, other: Self) -> Natural {
+        school(self, other)
     }
 }
 
-#[inline]
-fn mul_1digit(lhs: &Natural, rhs: Digit, offset: Digit) -> Natural {
-    let mut answer = Natural::new(offset + lhs.length + 1);
+fn school(lhs: &Natural, rhs: &Natural) -> Natural {
+    let mut answer = Natural::new(needed_capacity(lhs, rhs));
 
-    let mut digit = [0, 0];
-    for i in 0..lhs.capacity {
-        digit = mul_carry(lhs[i], rhs, digit[1]);
-        answer[offset + i] = digit[0];
+    for i in 0..rhs.length {
+        if rhs[i] == 0 {
+            continue;
+        }
+        let mut internal_answer = [0, 0];
+        for j in 0..(lhs.length + 1) {
+            internal_answer = mul_carry(rhs[i], lhs[j], internal_answer[1], answer[i + j]);
+            answer[i + j] = internal_answer[0];
+        }
     }
 
     answer.normalize();
@@ -37,5 +31,5 @@ fn mul_1digit(lhs: &Natural, rhs: Digit, offset: Digit) -> Natural {
 }
 
 fn needed_capacity(lhs: &Natural, rhs: &Natural) -> usize {
-    lhs.length + rhs.length // TODO: もうちょっと賢くする
+    lhs.length + rhs.length
 }
